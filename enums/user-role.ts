0885916b46a5581448486a97eaa6378fc95f83d0
kkn1125 <xpfjrnseks1@naver.com>
2025-08-14 00:36:37 +0900

    🛠️ fix: user role 수정

--- enums/user-role.ts
+++ enums/user-role.ts
@@ -6,3 +6,4 @@ export const UserRole = {
   Owner: 'owner', // CRUD + Invite 권한
 } as const;
 export type UserRole = (typeof UserRole)[keyof typeof UserRole];
+export const UserRoleList = Object.values(UserRole);
